8b5632555f8ef67ae4cfbc3ced4765ac55725eaf
Adhiansyah Ancha <69331906+adhiansyahancha@users.noreply.github.com>
2023-12-17 10:33:18 +0700

    Lupa ekspor fungsi

--- masalah001/solusi-pie.js
+++ masalah001/solusi-pie.js
@@ -8,8 +8,6 @@ const sigmaKelipatan = (n, k) => {
 	return k * p * (p + 1) / 2;
 };
 
-const jumlahKelipatan3atau5 = (batas) => {
+export const jumlahKelipatan3atau5 = (batas) => {
 	return sigmaKelipatan(batas, 3) + sigmaKelipatan(batas, 5) - sigmaKelipatan(batas, 15);
 };
-
-console.log(jumlahKelipatan3atau5(20));
